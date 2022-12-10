f3596bf5a182c2367de06c81c6487ef225a70f96
dievilla <2021018523@unfv.edu.pe>
2022-12-10 09:01:45 -0500

    first commit

---
+++ promedio.py
@@ -0,0 +1,16 @@
+
+
+if __name__ == '__main__':
+	n = int()
+	p = int()
+	z = int()
+	resultado = float()
+	print(" ingrese cebiche de conchas negras")
+	n = int(input())
+	print(" ingrese cebiche de pescado a la crema")
+	p = int(input())
+	print(" ingrese chicharon de calamar")
+	z = int(input())
+	resultado = (n+p+z)/3
+	print(" El promedio es: ",resultado)
+
